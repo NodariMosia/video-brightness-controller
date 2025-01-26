0b9cdbcc0f7e8ddc736831e39e90684ff60e7089
Nodar Mosia <nodarmosia@gmail.com>
2025-01-26 06:31:51 +0400

    remove unnecessary doc comments from custom types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,12 +1,6 @@
 declare global {
-    /**
-     * Currently supported media tags by the extension.
-     */
     type SupportedMediaTag = "video" | "img";
 
-    /**
-     * Settings object for the extension.
-     */
     type BrightnessControllerSettings = Record<SupportedMediaTag, number>;
 
     type MediaBrightnessController = {
